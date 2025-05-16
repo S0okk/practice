56623d6a0f0e5d85d0f3744177ffbf9b7506be30
Nikita <sso0kk207@gmail.com>
2025-05-16 19:21:38 +0300

    update

--- ege.py
+++ ege.py
@@ -4,8 +4,8 @@
 # s = open('k7b-6.txt').readline()
 # print(len('DAF' * 56 + 'D'))
 
-# s = open('24-171.txt').readline()
-# print(len('YZ'+'XYZ'*6 + 'XY')) ########
+# s = open('24-171.txt').read()
+# print(len('Z'+'XYZ'*17 + 'Z')) ########
 
 # s = open('24-197.txt').readline()
 # s = s.replace('ZXY', '*').replace('ZYX', '*')
@@ -24,17 +24,20 @@
 # s = s.split()
 # print(max(len(i) for i in s))
 
-# s = open('24-215.txt').readline()
-# for letter in 'ABC':
-#     s=s.replace(letter, '*')
-# for letter in '123':
-#     s=s.replace(letter, '&')
-# s = s.replace('*&*', '^')
-# for i in s:
-#     if i != '^':
-#         s = s.replace(i, ' ')
-# s = s.split()
-# print(max(len(x) for x in s)) ###########
+s = open('24-215.txt').readline()
+c=0
+ma = 0
+for letter in 'BC':
+    s=s.replace(letter, 'A')
+for letter in '23':
+    s=s.replace(letter, '1')
+for i in range(len(s)-2):
+    if s[i] + s[i+1] + s[i+2] == 'A1A':
+        c+=1
+        ma = max(ma, c)
+    elif c != 0:
+        c = 0
+print(ma)
 
 # from string import printable
 # s = open('24-279.txt').readline()
